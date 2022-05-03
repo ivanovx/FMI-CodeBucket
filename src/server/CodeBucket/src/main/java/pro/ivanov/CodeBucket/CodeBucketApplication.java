fb5263e90a4ab113426fc8eb1eed891f6f4e52d0
Ivan Ivanov <csyntax@outlook.com>
2022-05-03 18:09:51 +0300

    init spring app

---
+++ src/server/CodeBucket/src/main/java/pro/ivanov/CodeBucket/CodeBucketApplication.java
@@ -0,0 +1,13 @@
+package pro.ivanov.CodeBucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeBucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeBucketApplication.class, args);
+	}
+
+}
---
+++ src/server/CodeBucket/src/test/java/pro/ivanov/CodeBucket/CodeBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.ivanov.CodeBucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeBucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
