3d755340d7f3a1e39dbb3425f37ab73aa3753db0
Ivan Ivanov <ivancivanovbg@gmail.com>
2022-05-28 18:27:55 +0300

    remove old JAVA backend

--- src/old-server/src/main/java/pro/ivanov/CodeBucket/repositories/BucketRepository.java
+++
@@ -1,7 +0,0 @@
-package pro.ivanov.CodeBucket.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import pro.ivanov.CodeBucket.models.Bucket;
-
-public interface BucketRepository extends CrudRepository<Bucket, Integer> { }
